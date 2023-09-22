8fa3f813440bf58f30d6eb18ba17f28d7d3bf2a7
Aman Saurav <asaurav@falabella.cl>
2023-09-22 13:03:49 +0530

    Is subseqence problem.

--- src/main/java/Entrypoint.java
+++ src/main/java/Entrypoint.java
@@ -1,12 +1,14 @@
 import top_interview_questions.leet_code.array.*;
+import top_interview_questions.leet_code.daily_problem.IsSubsequence;
 import top_interview_questions.leet_code.daily_problem.MedianOfTwoSortedArray;
 
 public class Entrypoint {
 
     public static void main(String[] args) {
 
-        MedianOfTwoSortedArray numbersWithEvenDigits = new MedianOfTwoSortedArray();
-        System.out.println(numbersWithEvenDigits.findMedianSortedArrays(new int[]{}, new int[]{3,4}));
+        IsSubsequence numbersWithEvenDigits = new IsSubsequence();
+//        System.out.println(numbersWithEvenDigits.findMedianSortedArrays(new int[]{}, new int[]{3,4}));
+        numbersWithEvenDigits.isSubsequence("", "ahbgdc");
 
 
     }
